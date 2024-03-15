94a4f8d748b7f168eeb0fc0ecbf4c006612c2a18
Crowdbotics <engineering@crowdbotics.com>
2024-03-15 02:07:54 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "amazon-clone-3",
+  url: "https://amazon-clone-3.botics.co",
+  api: "https://amazon-clone-3.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
